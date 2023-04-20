58b0c993ff7271b338440dc0dba9b75bbef6cca7
Robert <robertlstevenson01@gmail.com>
2023-04-20 13:11:33 +0000

    remade the package

--- src/bno085/setup.py
+++ src/bno085/setup.py
@@ -4,7 +4,7 @@ package_name = 'bno085'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='Robert',
     maintainer_email='robertlstevenson01@gmail.com',
-    description='ROS2 Node for interface via I2C with a BNO085 Sensor and publish it's data',
+    description='ROS2 Node for interface via I2C with a BNO085 Sensor and publish the IMU msg data and the Sensor orientation (roll, pitch, yaw/heading) ',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'bno085_publisher = bno085.BNO085_pub:main',
         ],
     },
 )
